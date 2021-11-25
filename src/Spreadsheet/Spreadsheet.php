2b5148476d1d6639af53e2656a7232b56a5c6f2c
tomocky1 <siokobu8400@gmail.com>
2021-11-25 21:27:56 +0900

    ２回目コミット

--- src/Spreadsheet/Spreadsheet.php
+++ src/Spreadsheet/Spreadsheet.php
@@ -6,10 +6,23 @@ interface Spreadsheet
 {
     Const EXCEL = "EXCEL";
 
+    /**
+     * 対象のテーブルを登録するためのシートを作成し、カラムを入力しておく
+     * @param string $table シートを作成する対象のテーブル名
+     * @param array $columns 作成したシートに入力するカラム
+     * @return void
+     */
     public function createSheet($table, $columns);
 
+    /**
+     * 引数 $datas で受け取ったデータをスプレッドシート上に保存する
+     * @param $tableName string 保存先のスプレッドシートのパス
+     * @param $datas array スプレッドシートに書き込むデータ
+     * @throws
+     */
     public function setTableDatas($tableName, $datas);
 
+    
     public function deleteAllSheets();
 
     public function getAllTables();
